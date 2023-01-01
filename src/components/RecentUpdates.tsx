45abf4c8a174ffd0637d3f5eec048159848a4a4a
ed <ed@homex.com>
2023-01-01 12:46:00 -0500

    added useful links

--- src/components/RecentUpdates.tsx
+++ src/components/RecentUpdates.tsx
@@ -10,16 +10,31 @@ function preventDefault(event: React.MouseEvent) {
 export default function RecentUpdates() {
   return (
     <React.Fragment>
-      <Title>Recent Updates</Title>
+      <Title>Useful Links</Title>
       <Typography component="p" variant="h4">
-        Working on it bitch
+        Wowza
       </Typography>
       <Typography color="text.secondary" sx={{ flex: 1 }}>
-        Missing latest update
+        Site data is 1 patch behind
       </Typography>
       <div>
-        <Link color="primary" href="#" onClick={preventDefault}>
+        <Link color="primary" href="https://www.ageofempires.com/games/aoeiide/" target="_blank">
           Official AoE2 News
+        </Link>
+      </div>
+      <div>
+        <Link color="primary" href="https://aoe2recs.com/" target="_blank">
+          aoe2recs.com
+        </Link>
+      </div>
+      <div>
+        <Link color="primary" href="https://liquipedia.net/ageofempires/Main_Page" target="_blank">
+          liquipedia.net
+        </Link>
+      </div>
+      <div>
+        <Link color="primary" href="https://aoe2.gg/" target="_blank">
+          aoe2.gg
         </Link>
       </div>
     </React.Fragment>
