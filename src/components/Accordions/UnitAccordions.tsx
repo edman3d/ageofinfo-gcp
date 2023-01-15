f02feb2683c796d87635d79cb3c34ed745b9cf5a
ed <ed@homex.com>
2023-01-15 13:30:44 -0500

    unitaccordions prog

---
+++ src/components/Accordions/UnitAccordions.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import { useContext, useState } from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { UnitContext } from "../../contexts/UnitContext";
+// should take in a string and return 1 or more accordions
+
+type UnitAccordionsProps = {
+  unique_units: string | null;
+};
+
+export default function UnitAccordions(props: UnitAccordionsProps) {
+  const allUnits = useContext(UnitContext);
+  const units = useState(props.unique_units ? props.unique_units.split(";") : []);
+  console.log(`units accordion found ${units.length} units`);
+
+  return (
+    <div>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit
+            leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel2a-content" id="panel2a-header">
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit
+            leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion disabled>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel3a-content" id="panel3a-header">
+          <Typography>Disabled Accordion</Typography>
+        </AccordionSummary>
+      </Accordion>
+    </div>
+  );
+}
--- src/components/CivCompareDetails.tsx
+++ src/components/CivCompareDetails.tsx
@@ -10,6 +10,8 @@ import { TechContext } from "../contexts/TechContext";
 
 import { LIGHT_TAN_COLOR, MEDIUM_TAN_COLOR, DARK_TAN_COLOR } from "../constants/colors";
 
+import UnitAccordions from "./Accordions/UnitAccordions";
+
 const marginBottom = "10px"; // margin between stat rows
 const paddingLeft = "0px";
 
@@ -28,100 +30,99 @@ export default function CivCompareDetails(props: CivCompareDetailsProps) {
 
   return (
     <Card sx={{ height: 750, backgroundColor: LIGHT_TAN_COLOR }}>
-      <CardActionArea>
-        <div style={{ display: "flex", alignItems: "center", paddingLeft: 4, paddingTop: 4 }}>
-          <CardMedia
-            component="img"
-            height="140"
-            image={
-              props.civ
-                ? require(`../images/civilizations/${props.civ?.image}.png`)
-                : require("../images/staticons/convert.png")
-            }
-            alt="civ banner"
-            sx={{ height: "64px", width: "64px" }}
-          />
-          <Typography variant="h5" sx={{ marginLeft: 1 }}>
-            {props.civ ? props.civ.name : "Select a Civilization"}
-          </Typography>
-        </div>
-        <CardContent sx={{ height: 750 }}>
-          {props.civ ? (
-            <>
-              <Typography variant="body2" color="text.secondary">
-                Army Type
-              </Typography>
-              <Typography
-                variant="subtitle2"
-                color="text.secondary"
-                sx={{ marginBottom: marginBottom, paddingLeft: paddingLeft }}
-              >
-                <DetailsList delimitedString={props.civ.army_type} />
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Expansion
-              </Typography>
-              <Typography
-                variant="subtitle2"
-                color="text.secondary"
-                sx={{ marginBottom: marginBottom, paddingLeft: paddingLeft }}
-              >
-                <DetailsList delimitedString={props.civ.expansion} />
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Unique Unit/s
-              </Typography>
-              <Typography
-                variant="subtitle2"
-                color="text.secondary"
-                sx={{ marginBottom: marginBottom, paddingLeft: paddingLeft }}
-              >
-                <DetailsList delimitedString={props.civ.unique_unit} />
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Unique Techs (could make these a dropdown with tech info)
-              </Typography>
-              <Typography
-                variant="subtitle2"
-                color="text.secondary"
-                sx={{ marginBottom: marginBottom, paddingLeft: paddingLeft }}
-              >
-                <DetailsList delimitedString={props.civ.unique_tech} />
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Team Bonus
-              </Typography>
-              <Typography
-                variant="subtitle2"
-                color="text.secondary"
-                sx={{ marginBottom: marginBottom, paddingLeft: paddingLeft }}
-              >
-                <DetailsList delimitedString={props.civ.team_bonus} />
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Civ Bonus
-              </Typography>
-              <Typography
-                variant="subtitle2"
-                color="text.secondary"
-                sx={{ marginBottom: marginBottom, paddingLeft: paddingLeft }}
-              >
-                <DetailsList delimitedString={props.civ.civilization_bonus} />
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Unique Building/s
-              </Typography>
-              <Typography variant="subtitle2" color="text.secondary">
-                {props.civ.unique_buildings.length > 0 ? (
-                  <DetailsList delimitedString={props.civ.unique_buildings} />
-                ) : (
-                  <DetailsList delimitedString={"None"} />
-                )}
-              </Typography>
-            </>
-          ) : null}
-        </CardContent>
-      </CardActionArea>
+      <div style={{ display: "flex", alignItems: "center", paddingLeft: 4, paddingTop: 4 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={
+            props.civ
+              ? require(`../images/civilizations/${props.civ?.image}.png`)
+              : require("../images/staticons/convert.png")
+          }
+          alt="civ banner"
+          sx={{ height: "64px", width: "64px" }}
+        />
+        <Typography variant="h5" sx={{ marginLeft: 1 }}>
+          {props.civ ? props.civ.name : "Select a Civilization"}
+        </Typography>
+      </div>
+      <CardContent sx={{ height: 750 }}>
+        {props.civ ? (
+          <>
+            <Typography variant="body2" color="text.secondary">
+              Army Type
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              color="text.secondary"
+              sx={{ marginBottom: marginBottom, paddingLeft: paddingLeft }}
+            >
+              <DetailsList delimitedString={props.civ.army_type} />
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Expansion
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              color="text.secondary"
+              sx={{ marginBottom: marginBottom, paddingLeft: paddingLeft }}
+            >
+              <DetailsList delimitedString={props.civ.expansion} />
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Unique Unit/s
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              color="text.secondary"
+              sx={{ marginBottom: marginBottom, paddingLeft: paddingLeft }}
+            >
+              <DetailsList delimitedString={props.civ.unique_unit} />
+              <UnitAccordions unique_units={props.civ.unique_unit} />
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Unique Techs (could make these a dropdown with tech info)
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              color="text.secondary"
+              sx={{ marginBottom: marginBottom, paddingLeft: paddingLeft }}
+            >
+              <DetailsList delimitedString={props.civ.unique_tech} />
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Team Bonus
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              color="text.secondary"
+              sx={{ marginBottom: marginBottom, paddingLeft: paddingLeft }}
+            >
+              <DetailsList delimitedString={props.civ.team_bonus} />
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Civ Bonus
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              color="text.secondary"
+              sx={{ marginBottom: marginBottom, paddingLeft: paddingLeft }}
+            >
+              <DetailsList delimitedString={props.civ.civilization_bonus} />
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Unique Building/s
+            </Typography>
+            <Typography variant="subtitle2" color="text.secondary">
+              {props.civ.unique_buildings.length > 0 ? (
+                <DetailsList delimitedString={props.civ.unique_buildings} />
+              ) : (
+                <DetailsList delimitedString={"None"} />
+              )}
+            </Typography>
+          </>
+        ) : null}
+      </CardContent>
     </Card>
   );
 }
