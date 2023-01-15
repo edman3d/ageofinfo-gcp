93478d84b7d786ee4d878449f221adca49624418
ed <ed@homex.com>
2023-01-15 12:36:23 -0500

    convert orders to context

--- src/components/Orders.tsx
+++ src/components/Orders.tsx
@@ -6,28 +6,15 @@ import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Title from "./Title";
-import type { Civilization, Technology, Unit } from "../types";
-let civs: Civilization[] = require("./../data/civs.json");
-console.log(`Found ${civs.length} civs`);
-
-// Generate Order Data
-// function createData(id: number, date: string, name: string, shipTo: string, paymentMethod: string, amount: number) {
-//   return { id, date, name, shipTo, paymentMethod, amount };
-// }
-
-// const rows = [
-//   createData(0, "16 Mar, 2019", "Elvis Presley", "Tupelo, MS", "VISA ⠀•••• 3719", 312.44),
-//   createData(1, "16 Mar, 2019", "Paul McCartney", "London, UK", "VISA ⠀•••• 2574", 866.99),
-//   createData(2, "16 Mar, 2019", "Tom Scholz", "Boston, MA", "MC ⠀•••• 1253", 100.81),
-//   createData(3, "16 Mar, 2019", "Michael Jackson", "Gary, IN", "AMEX ⠀•••• 2000", 654.39),
-//   createData(4, "15 Mar, 2019", "Bruce Springsteen", "Long Branch, NJ", "VISA ⠀•••• 5919", 212.79),
-// ];
+import { useContext } from "react";
+import { CivContext } from "../contexts/CivContext";
 
 function preventDefault(event: React.MouseEvent) {
   event.preventDefault();
 }
 
 export default function Orders() {
+  const civs = useContext(CivContext);
   return (
     <React.Fragment>
       <Title>Civilization Details</Title>
@@ -41,7 +28,7 @@ export default function Orders() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {civs.map((civ) => (
+          {civs?.map((civ) => (
             <TableRow key={civ.name}>
               <TableCell>{civ.name}</TableCell>
               <TableCell>{civ.army_type}</TableCell>
