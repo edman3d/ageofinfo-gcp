40e2ac192498976718fe4d33ddf4dd8513f0685f
ed <ed@homex.com>
2023-01-27 15:59:47 -0500

    delete shit

--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -123,18 +123,6 @@ function DashboardContent() {
                     pr: "24px", // keep right padding when drawer closed
                   }}
                 >
-                  {/* <IconButton
-                    edge="start"
-                    color="inherit"
-                    aria-label="open drawer"
-                    onClick={toggleDrawer}
-                    sx={{
-                      marginRight: "36px",
-                      ...(open && { display: "none" }),
-                    }}
-                  >
-                    <MenuIcon />
-                  </IconButton> */}
                   <Typography component="h1" variant="h6" color="black" noWrap sx={{ flexGrow: 1 }}>
                     AgeOfInfo Dashboard
                   </Typography>
@@ -145,26 +133,6 @@ function DashboardContent() {
                   </IconButton>
                 </Toolbar>
               </AppBar>
-              {/* <Drawer variant="permanent" open={open}>
-                <Toolbar
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "flex-end",
-                    px: [1],
-                  }}
-                >
-                  <IconButton onClick={toggleDrawer}>
-                    <ChevronLeftIcon />
-                  </IconButton>
-                </Toolbar>
-                <Divider />
-                <List component="nav">
-                  {mainListItems}
-                  <Divider sx={{ my: 1 }} />
-                  {secondaryListItems}
-                </List>
-              </Drawer> */}
               <Box
                 component="main"
                 sx={{
@@ -177,46 +145,12 @@ function DashboardContent() {
                 <Toolbar />
                 <Container maxWidth="xl" sx={{ mt: 4, mb: 4, backgroundColor: DARK_TAN_COLOR }}>
                   <Grid container spacing={3}>
-                    {/* Chart */}
-                    {/* <Grid item xs={12} md={8} lg={9}>
-                      <Paper
-                        sx={{
-                          p: 2,
-                          display: "flex",
-                          flexDirection: "column",
-                          height: 240,
-                          backgroundColor: MEDIUM_TAN_COLOR,
-                        }}
-                      >
-                        <Chart />
-                      </Paper>
-                    </Grid> */}
-                    {/* Recent Deposits */}
-                    {/* <Grid item xs={12} md={4} lg={3}>
-                      <Paper
-                        sx={{
-                          p: 2,
-                          display: "flex",
-                          flexDirection: "column",
-                          height: 240,
-                          backgroundColor: MEDIUM_TAN_COLOR,
-                        }}
-                      >
-                        <RecentUpdates />
-                      </Paper>
-                    </Grid> */}
                     {/* Civilization Compare Section */}
                     <Grid item xs={12}>
                       <Paper sx={{ p: 2, display: "flex", flexDirection: "column", backgroundColor: MEDIUM_TAN_COLOR }}>
                         <CivCompare />
                       </Paper>
                     </Grid>
-                    {/* Civilization Data Grid */}
-                    {/* <Grid item xs={12}>
-                      <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-                        <DataGridCivs />
-                      </Paper>
-                    </Grid> */}
                   </Grid>
                   <Copyright sx={{ pt: 4 }} />
                 </Container>
