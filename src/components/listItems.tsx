18183b061772454f06cfa41cede867ca0ceae031
ed <ed@homex.com>
2023-02-04 13:19:17 -0500

    use mui with react router

--- src/components/listItems.tsx
+++ src/components/listItems.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+import ListItemLink from "@mui/material/List";
 import ListSubheader from "@mui/material/ListSubheader";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
@@ -9,16 +10,16 @@ import PeopleIcon from "@mui/icons-material/People";
 import BarChartIcon from "@mui/icons-material/BarChart";
 import LayersIcon from "@mui/icons-material/Layers";
 import AssignmentIcon from "@mui/icons-material/Assignment";
-import { LIGHT_TAN_COLOR, MEDIUM_TAN_COLOR, DARK_TAN_COLOR } from "../constants/colors";
-import { Link } from "react-router-dom";
-
+import { LIGHT_TAN_COLOR, MEDIUM_TAN_COLOR, DARK_TAN_COLOR, TEXT_COLOR_MAIN } from "../constants/colors";
+import { Link as RouterLink } from "react-router-dom";
+import Link from "@mui/material/Link";
 export const mainListItems = (
   <React.Fragment>
     <ListSubheader component="div" inset sx={{ backgroundColor: LIGHT_TAN_COLOR }}>
       Tools
     </ListSubheader>
 
-    <Link to="/">
+    <Link component={RouterLink} to="/" style={{ textDecoration: "none", color: TEXT_COLOR_MAIN }}>
       <ListItemButton>
         <ListItemIcon>
           <DashboardIcon />
@@ -27,7 +28,7 @@ export const mainListItems = (
       </ListItemButton>
     </Link>
 
-    <Link to="civdata">
+    <Link component={RouterLink} to="civdata" style={{ textDecoration: "none", color: TEXT_COLOR_MAIN }}>
       <ListItemButton>
         <ListItemIcon>
           <BarChartIcon />
@@ -43,12 +44,14 @@ export const secondaryListItems = (
     <ListSubheader component="div" inset sx={{ backgroundColor: LIGHT_TAN_COLOR }}>
       Data Tables
     </ListSubheader>
+
     <ListItemButton>
       <ListItemIcon>
         <PeopleIcon />
       </ListItemIcon>
       <ListItemText primary="Units" />
     </ListItemButton>
+
     <ListItemButton>
       <ListItemIcon>
         <ShoppingCartIcon />
--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,3 +1,4 @@
 export const LIGHT_TAN_COLOR = "#e5b68b";
 export const MEDIUM_TAN_COLOR = "#CDA581";
 export const DARK_TAN_COLOR = "#a17044";
+export const TEXT_COLOR_MAIN = "#202124";
