4618077a80788fa51bbeecb6ab7c97475d144348
edman3d <ejsmajestic@gmail.com>
2023-08-15 13:52:30 -0400

    more restrictive unit type

--- src/types/Unit.ts
+++ src/types/Unit.ts
@@ -3,27 +3,27 @@ export type Unit = {
   id: number;
   description: string;
   expansion: string;
-  age: string;
-  created_in: string;
-  cost: string;
-  build_time: string;
+  age: "N/A" | "Dark" | "Feudal" | "Castle" | "Imperial";
+  created_in: string; // some units can be created in multiple bldgs (e.g. Castle;Donjon)
+  cost: string; // not a Cost type because data isnt JSON by default (e.g. {Food: 2; Wood: 2} instead of {Food: 2, Wood: 2})
+  build_time: string; // string type because some are "N/A"
   reload_time: number;
   attack_delay: number;
   movement_rate: number;
   line_of_sight: number;
   hit_points: number;
-  range: string;
+  range: string; // string type because some units have a min range (e.g. 5-12)
   attack: number;
-  armor: string;
+  armor: string; // string type because its currently represented as melee armor/ranged armor (e.g 2/5)
   attack_bonus?: CombatBonus[];
   armor_bonus?: CombatBonus[];
   search_radius: number;
   accuracy: number;
   blast_radius: number;
   image: string;
-  type: string;
+  type: "Cheat" | "Civilian" | "Gaia" | "Military" | "Navy" | "Unbuildable";
   armor_class: string;
-  special: string;
+  special?: string;
 };
 
 export type CombatBonus = {
