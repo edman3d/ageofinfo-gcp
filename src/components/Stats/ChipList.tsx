ce6fa81ceeb7ededd3c39ff3d3c31bd0a2e0f341
edman3d <ejsmajestic@gmail.com>
2023-08-20 09:57:00 -0400

    refine ChipList

--- src/components/Stats/ChipList.tsx
+++ src/components/Stats/ChipList.tsx
@@ -1,13 +1,23 @@
 import * as React from "react";
 import { Chip, Stack } from "@mui/material";
 
+/**
+ * e.g. Army Type - `'Elephants;Ships'`
+ *
+ * e.g. Armor Class - `'Infantry;Unique Unit'`
+ */
 type ChipListProps = {
   delimitedString: string;
 };
 
+/**
+ * Renders one or more `<Chip/>` components. Creates 1 `<Chip/>` per delimited string.
+ * @param props
+ * @returns a `<Stack/>` of `<Chip/>` components
+ */
 export function ChipList(props: ChipListProps) {
-  const listItems = props.delimitedString.trim().split(";");
-  let hasAtLeastOneValue = props.delimitedString.length > 0 && listItems.length > 0;
+  const listItems: string[] = props.delimitedString.trim().split(";");
+  const hasAtLeastOneValue: boolean = props.delimitedString.length > 0 && listItems.length > 0;
 
   return (
     <Stack spacing={0.5} marginBottom={1}>
