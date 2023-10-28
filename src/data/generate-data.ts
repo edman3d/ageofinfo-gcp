2c366cc9a9085f487d00594eec496a20a460e01a
edman3d <ejsmajestic@gmail.com>
2023-10-28 12:24:21 -0400

    parsing and writing data works

--- src/data/generate-data.ts
+++ src/data/generate-data.ts
@@ -1,29 +1,47 @@
-/** csv file
-a,b,c
-1,2,3
-4,5,6
-*/
-const csvFilePath = "src/data/rawData/aoe2_de_v3_units.csv";
+const fs = require("fs");
 const csv = require("csvtojson/v2");
-// csv()
-//   .fromFile(csvFilePath)
-//   .then((jsonObj) => {
-//     console.log(jsonObj);
-//     /**
-//      * [
-//      * 	{a:"1", b:"2", c:"3"},
-//      * 	{a:"4", b:"5". c:"6"}
-//      * ]
-//      */
-//   });
 
-// Async / await usage
-// const jsonArray = await csv().fromFile(csvFilePath);
-// console.log("derp");
-// console.log(jsonArray);
-async function getData() {
-  const jsonArray = await csv().fromFile(csvFilePath);
-  console.log(jsonArray);
+const unitCsvFilePath = "src/data/rawData/aoe2_de_v3_units.csv";
+const techsCsvFilePath = "src/data/rawData/aoe2_de_v3_techs.csv";
+const buildingsCsvFilePath = "src/data/rawData/aoe2_de_v3_buildings.csv";
+const civsCsvFilePath = "src/data/rawData/aoe2_de_v3_civs.csv";
+
+async function parseAndWriteData() {
+  // Parse and write Units
+  const unitsArray = await csv().fromFile(unitCsvFilePath);
+  fs.writeFile("src/data/rawData/units.json", JSON.stringify(unitsArray), (err) => {
+    if (err) console.log(err);
+    else {
+      console.log("Units file written successfully\n");
+    }
+  });
+
+  // Parse and write Technologies
+  const techsArray = await csv().fromFile(techsCsvFilePath);
+  fs.writeFile("src/data/rawData/techs.json", JSON.stringify(techsArray), (err) => {
+    if (err) console.log(err);
+    else {
+      console.log("Techs file written successfully\n");
+    }
+  });
+
+  // Parse and write Buildings
+  const buildingsArray = await csv().fromFile(buildingsCsvFilePath);
+  fs.writeFile("src/data/rawData/buildings.json", JSON.stringify(buildingsArray), (err) => {
+    if (err) console.log(err);
+    else {
+      console.log("Buildings file written successfully\n");
+    }
+  });
+
+  // Parse and write Civilizations
+  const civsArray = await csv().fromFile(civsCsvFilePath);
+  fs.writeFile("src/data/rawData/civs.json", JSON.stringify(civsArray), (err) => {
+    if (err) console.log(err);
+    else {
+      console.log("Civs file written successfully\n");
+    }
+  });
 }
 
-getData();
+parseAndWriteData();
