ffff887bb19085e77bb8a58701b6e3e14f7b945a
edman3d <ejsmajestic@gmail.com>
2023-10-29 11:41:23 -0400

    unit cost validation

--- src/__tests/dataValidation.test.ts
+++ src/__tests/dataValidation.test.ts
@@ -6,18 +6,19 @@ const units: Unit[] = require("./../data/units.json");
 
 describe("test unit data is valid", () => {
   test.each(units)("testing unit ($id, $name)", (unit) => {
-    if (unit.type !== "Gaia" && unit.type !== "Unbuildable") {
-      // Check that cost field is valid
+    // Check that cost field is valid
+    if (unit.cost) {
       expect(isValidJSON(unit.cost)).toBe(true);
+    }
 
-      // Check that attack_bonus field is valid
-      if (unit.attack_bonus) {
-        expect(isValidJSON(unit.attack_bonus.toString())).toBe(true);
-      }
+    // Check that attack_bonus field is valid
+    if (unit.attack_bonus) {
+      expect(isValidJSON(unit.attack_bonus.toString())).toBe(true);
+    }
 
-      if (unit.armor_bonus) {
-        expect(isValidJSON(unit.armor_bonus.toString())).toBe(true);
-      }
+    // Check that armor_bonus field is valid
+    if (unit.armor_bonus) {
+      expect(isValidJSON(unit.armor_bonus.toString())).toBe(true);
     }
   });
 });
