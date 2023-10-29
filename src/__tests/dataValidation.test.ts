5765ae05f55767bc98b1a0e11c9a910302d56d3a
edman3d <ejsmajestic@gmail.com>
2023-10-29 11:29:46 -0400

    valid json checker

--- src/__tests/dataValidation.test.ts
+++ src/__tests/dataValidation.test.ts
@@ -1,25 +1,26 @@
 /* eslint-disable jest/no-conditional-expect */
-import { type Building, type Civilization, type Technology, type Unit } from "../types";
+import { type Unit } from "../types";
 
 const units: Unit[] = require("./../data/units.json");
 
-describe("test unit data is valid", () => {
-  //   test.each(units)("testing unit ($id, $name)", ({ id, name, cost, type }) => {
-  //     if (type !== "Gaia" && type !== "Unbuildable") {
-  //       const costObj = JSON.parse(cost);
-  //       expect(costObj).toBeInstanceOf(Object);
-  //     }
-  //   });
+const isValidJSON = (str: string) => {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
 
+describe("test unit data is valid", () => {
   test.each(units)("testing unit ($id, $name)", (unit) => {
     if (unit.type !== "Gaia" && unit.type !== "Unbuildable") {
       // Check that cost field is valid
-      const costObj = JSON.parse(unit.cost);
-      expect(costObj).toBeInstanceOf(Object);
+      expect(isValidJSON(unit.cost)).toBe(true);
 
       // Check that attack_bonus field is valid
       if (unit.attack_bonus) {
-        const atkbonus = JSON.parse(unit.attack_bonus.toString());
+        expect(isValidJSON(unit.attack_bonus.toString())).toBe(true);
       }
     }
   });
